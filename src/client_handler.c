23cf563d7be26e6f54f1b7657f9b0bc21b056f6c
r0zbot <r0zbot@users.noreply.github.com>
2019-08-11 19:30:10 -0300

    Documented a few commands from proftpd

--- src/client_handler.c
+++ src/client_handler.c
@@ -21,7 +21,7 @@ start_client_handler(Socket *s)
 {
 	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
 	check_null(buffer, "client_handler: não foi possível alocar buffer");
-	socket_write(s, "220 BFTP - Batista's FTP Server ***\n");
+	socket_write(s, "220 BFTP - Batista's FTP Server [IP_ADDR]\n"); //TODO: Colocar endereço de IP
 	
 	char *user = malloc(sizeof(char) * MAX_USER_LENGTH);
 	char *pass = malloc(sizeof(char) * MAX_PASS_LENGTH);
@@ -32,11 +32,11 @@ start_client_handler(Socket *s)
 		if (strncmpi(buffer, "USER", 4) == 0) {
 			strncpy(user, buffer + 4, MAX_USER_LENGTH);
 			pass[0] = '\0';
-			// TODO: reproduzir respostas do proftpd
 			fsocket_write(s, "331 Password required for %s\n", user);
 		}
 		else if (strncmpi(buffer, "PASS", 4) == 0) {
-			if (!strlen(user)) socket_write(s, "envie usuário primeiro\n");
+			if (!strlen(user)) socket_write(s, "503 Login with USER first\n");
+			else if (strncmp(buffer+4, "teste123", MAX_PASS_LENGTH) != 0) socket_write(s, "530 Login incorrect.\n");
 			else {
 				strncpy(pass, buffer + 4, MAX_PASS_LENGTH);
 				socket_write(s, "230 User %s logged in\n", user);
@@ -44,14 +44,15 @@ start_client_handler(Socket *s)
 		}
 		// TODO: todos outros comandos possíveis
 		else if (strncmpi(buffer, "QUIT", 4) == 0) {
-			socket_write(s, "221 até a próxima...\n");
+			socket_write(s, "221 Goodbye.\n");
 			socket_fin(s);
-			break;
 		}
         else if (strncmpi(buffer, "DEBUG", 5) == 0) {
             fsocket_write(s, "User: %s", user);
             fsocket_write(s, "Pass: %s", pass);
-            break;
+        }
+        else{
+            fsocket_write(s, "500 %s not understood\n", buffer);
         }
 		
 		/* estado do login */
@@ -59,6 +60,53 @@ start_client_handler(Socket *s)
 		else logged = false;
 		
 	}
+	/* TODO - Missing commands:
+	    USER r0zbot
+	        331 Password required for r0zbot
+	    PASS senha
+	        230 User r0zbot logged in
+	    SYST
+	        215 UNIX Type: L8
+	    FEAT
+	        211-Features:
+            211-CLNT
+            211-EPRT
+            211-EPSV
+            211-HOST
+            211-LANG en-US.UTF-8*;en-US
+            211-MDTM
+            211-MFF modify;UNIX.group;UNIX.mode;
+            211-MFMT
+            211-MLST modify*;perm*;size*;type*;unique*;UNIX.group*;UNIX.groupname*;UNIX.mode*;UNIX.owner*;UNIX.ownername*;
+            211-REST STREAM
+            211-SITE COPY
+            211-SITE MKDIR
+            211-SITE RMDIR
+            211-SITE SYMLINK
+            211-SITE UTIME
+            211-SIZE
+            211-TVFS
+            211-UTF8
+            211 End
+	    CLNT Cliente_de_FTP 1.0.0
+	        200 OK
+	    OPTS UTF8 ON
+	        200 UTF8 set to on
+	    PWD
+	        257 "/home/r0zbot" is the current directory
+	    TYPE A
+	        200 Type set to A
+	    PASV
+	        227 Entering Passive Mode (192,168,195,129,131,35).
+	    MLSD
+	        150 Opening ASCII mode data connection for MLSD
+	        226 Transfer complete
+	    TYPE A
+            200 Type set to A
+	    REST 0
+	        350 Restarting at 0. Send STORE or RETRIEVE to initiate transfer
+	        421 No transfer timeout (600 seconds): closing control connection
+	 */
 	free(user);
 	free(pass);
 	exit(0);
